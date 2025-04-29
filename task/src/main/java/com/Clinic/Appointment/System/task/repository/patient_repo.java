a3c4f26f325bbaf244afcf621d1373bca2c97b90
Rajavel <76117631+rajavel-sa@users.noreply.github.com>
2025-04-29 15:12:44 +0530

    Delete task directory

--- task/src/main/java/com/Clinic/Appointment/System/task/repository/patient_repo.java
+++
@@ -1,6 +0,0 @@
-package com.Clinic.Appointment.System.task.repository;
-
-import com.Clinic.Appointment.System.task.model.patient_mod;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface patient_repo extends JpaRepository<patient_mod, String> {}
